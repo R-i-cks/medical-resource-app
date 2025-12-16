ac4a0453350b46212b2cdc700fe99ec7ee8f6f56
Ricardo Araújo <92271829+R-i-cks@users.noreply.github.com>
2025-12-16 20:29:00 +0000

    Update and rename run.py to app.py

--- app.py
+++ app.py
@@ -663,6 +663,4 @@ def qa():
 #app.run(host="localhost", port=4002, debug=True)
 
 if __name__ == '__main__':
-
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run()
